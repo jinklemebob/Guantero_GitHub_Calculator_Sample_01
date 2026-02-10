1dcfdb98a5f3f632fbce71b12388d98bf3e67c88
Valderrama, Neilshawn Rafael (Student) <valderrama.403100@balagtas.sti.edu.ph>
2026-02-10 19:59:40 +0800

    feat: division function

---
+++ Guantero_GitHub_Calculator_Sample/Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guantero_GitHub_Calculator_Sample
+{
+    internal class Division
+    {
+        private double total;
+        private double num1;
+        private double num2;
+        public double division(double num1, double num2)
+        {
+            return this.total = num1 / num2; //this function divides two numbers and returns the result
+        }
+        
+        
+    }
+}
