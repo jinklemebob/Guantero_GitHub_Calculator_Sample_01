7b3e3a0055873290783c49f1e4932b10daaa9ff4
jinklemebob <guantero.290593@balagtas.sti.edu.ph>
2026-02-10 19:57:25 +0800

    FEAT: Added Multiplication Operator.

---
+++ Guantero_GitHub_Calculator_Sample/Multiplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guantero_GitHub_Calculator_Sample
+{
+    internal class Multiplication
+    {
+       public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+            // returns the values of num1 and num2 and multiplies them together
+        }
+    }
+}
